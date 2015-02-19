478b63214115edad5f9ad9b16f8fb38ab8e3e033
accnex569 <accnex569@hotmail.com>
2015-02-19 14:56:49 -0430

    make it better

--- main.js
+++ main.js
@@ -11,19 +11,12 @@ app.listen(PORT, function() {
 	console.log("Listening on port " + PORT)
 });
 
-app.configure(function() {
-	app.set('view options', {
-		layout: false
-	});
+app.set('view engine', 'jade');
 	
 	app.use(express.static(__dirname + '/static'));
-});
 
-io.configure(function() {
-	io.disable('log');
-	
 	io.set('transports', ['websocket', 'xhr-polling']);
-});
+
 
 app.get('/', function(request, response) {
 	response.render('main.jade');
